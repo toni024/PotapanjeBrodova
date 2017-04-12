78653f69cec148e360f7b487683dea17c80157ae
Julijan Šribar <julijan.sribar@gmail.com>
2017-04-12 20:05:17 +0200

    Dodan brodograditelj i testovi za njega

---
+++ PotapanjeBrodova/PotapanjeBrodova/Brodograditelj.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public class Brodograditelj
+    {
+        public Flota SložiFlotu(Mreža mreža, IEnumerable<int> duljineBrodova)
+        {
+            Flota flota = new Flota();
+            TerminatorPolja terminator = new TerminatorPolja(mreža);
+            foreach (int i in duljineBrodova)
+            {
+                var nizovi = mreža.DajNizoveSlobodnihPolja(i);
+                int indeks = slučajni.Next(nizovi.Count());
+                var niz = nizovi.ElementAt(indeks);
+                flota.DodajBrod(niz);
+                terminator.UkloniPolja(niz);
+            }
+            return flota;
+        }
+
+        // TODO: obratiti pažnju na slučaj da se ne mogu svi brodovi složiti
+
+        private Random slučajni = new Random();
+    }
+}
---
+++ PotapanjeBrodova/Test/TestBrodograditelja.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test
+{
+    [TestClass]
+    public class TestBrodograditelja
+    {
+        [TestMethod]
+        public void Brodograditelj_SložiFlotuVraćaFlotuSBrodovimaZadaneDuljine()
+        {
+            Brodograditelj b = new Brodograditelj();
+            Mreža mreža = new Mreža(10, 10);
+            IEnumerable<int> duljineBrodova = new int[] { 5, 4, 4, 3, 3, 3, 2, 2, 2, 2 };
+            Flota f = b.SložiFlotu(mreža, duljineBrodova);
+            Assert.AreEqual(duljineBrodova.Count(), f.BrojBrodova);
+            // TODO: provjera ima li samo jedan brod duljine 5
+            // TODO: provjera ima li dva broda duljine 4...
+        }
+    }
+}
