b61fa34251c153b050f72b636ffa2318ee3f2556
Julijan Šribar <julijan.sribar@gmail.com>
2017-04-26 20:31:47 +0200

    Dodali smo interface IGađani. Implementirali smo IGađani za Brod i napisali testove.

--- PotapanjeBrodova/PotapanjeBrodova/Flota.cs
+++ PotapanjeBrodova/PotapanjeBrodova/Flota.cs
@@ -5,11 +5,16 @@ using System.Text;
 
 namespace PotapanjeBrodova
 {
-    public class Flota
+    public class Flota : IGađani
     {
         public void DodajBrod(IEnumerable<Polje> polja)
         {
             brodovi.Add(new Brod(polja));
+        }
+
+        public RezultatGađanja Gađaj(Polje polje)
+        {
+            throw new NotImplementedException();
         }
 
         public int BrojBrodova
