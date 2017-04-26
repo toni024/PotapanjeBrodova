3bf1f091b054c9f381dcfdb4689d24c39f10b2fd
Julijan Šribar <julijan.sribar@gmail.com>
2017-04-26 20:41:29 +0200

    Flota implementira sušelje IGađani. Napravljeni testovi.

--- PotapanjeBrodova/PotapanjeBrodova/Flota.cs
+++ PotapanjeBrodova/PotapanjeBrodova/Flota.cs
@@ -14,7 +14,13 @@ namespace PotapanjeBrodova
 
         public RezultatGađanja Gađaj(Polje polje)
         {
-            throw new NotImplementedException();
+            foreach (Brod brod in brodovi)
+            {
+                var rezultat = brod.Gađaj(polje);
+                if (rezultat != RezultatGađanja.Promašaj)
+                    return rezultat;
+            }
+            return RezultatGađanja.Promašaj;
         }
 
         public int BrojBrodova
