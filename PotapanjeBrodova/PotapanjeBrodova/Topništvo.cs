256484ea02157a27d4dded08b45ad519505ada61
Julijan Šribar <julijan.sribar@gmail.com>
2017-05-05 07:47:55 +0200

    Dodano Topništvo i testovi za njega. Dodani kosturi tri pucača.

---
+++ PotapanjeBrodova/PotapanjeBrodova/Topništvo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public enum TaktikaGađanja
+    {
+        Nasumično,
+        Kružno,
+        Linijsko
+    }
+
+    public class Topništvo
+    {
+        public Topništvo(int redaka, int stupaca, IEnumerable<int> duljineBrodova)
+        {
+            mreža = new Mreža(redaka, stupaca);
+            this.duljineBrodova = new List<int>(duljineBrodova);
+            TaktikaGađanja = TaktikaGađanja.Nasumično;
+        }
+
+        public void ObradiGađanje(RezultatGađanja rezultat)
+        {
+            if (rezultat == RezultatGađanja.Promašaj)
+                return;
+            if (rezultat == RezultatGađanja.Pogodak)
+            {
+                switch (TaktikaGađanja)
+                {
+                    case TaktikaGađanja.Nasumično:
+                        PromijeniTaktikuUKružno();
+                        return;
+                    case TaktikaGađanja.Kružno:
+                        PromijeniTaktikuULinijsko();
+                        return;
+                    case TaktikaGađanja.Linijsko:
+                        return;
+                    default:
+                        Debug.Assert(false);
+                        break;
+                }
+            }
+            Debug.Assert(rezultat == RezultatGađanja.Potopljen);
+            PromijeniTaktikuUNasumično();
+        }
+
+        private void PromijeniTaktikuUKružno()
+        {
+            TaktikaGađanja = TaktikaGađanja.Kružno;
+        }
+
+        private void PromijeniTaktikuULinijsko()
+        {
+            TaktikaGađanja = TaktikaGađanja.Linijsko;
+        }
+
+        private void PromijeniTaktikuUNasumično()
+        {
+            TaktikaGađanja = TaktikaGađanja.Nasumično;
+        }
+
+        public TaktikaGađanja TaktikaGađanja { get; private set; }
+
+        private Mreža mreža;
+        private List<int> duljineBrodova;
+    }
+}
---
+++ PotapanjeBrodova/Test/TestTopništva.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+
+namespace Test
+{
+    [TestClass]
+    public class TestTopništva
+    {
+        [TestMethod]
+        public void Topništvo_NaPočetkuJeTaktikaNasumična()
+        {
+            int redaka = 5;
+            int stupaca = 5;
+            int[] duljineBrodova = new int[] { 3, 2, 1 };
+            Topništvo t = new Topništvo(redaka, stupaca, duljineBrodova);
+            Assert.AreEqual(TaktikaGađanja.Nasumično, t.TaktikaGađanja);
+        }
+
+        [TestMethod]
+        public void Topništvo_NasumičnaTaktikaNakonPromašajaOstajeNasumična()
+        {
+            int redaka = 5;
+            int stupaca = 5;
+            int[] duljineBrodova = new int[] { 3, 2, 1 };
+            Topništvo t = new Topništvo(redaka, stupaca, duljineBrodova);
+            t.ObradiGađanje(RezultatGađanja.Promašaj);
+            Assert.AreEqual(TaktikaGađanja.Nasumično, t.TaktikaGađanja);
+        }
+
+        [TestMethod]
+        public void Topništvo_NasumičnaTaktikaNakonPotapanjaOstajeNasumična()
+        {
+            int redaka = 5;
+            int stupaca = 5;
+            int[] duljineBrodova = new int[] { 3, 2, 1 };
+            Topništvo t = new Topništvo(redaka, stupaca, duljineBrodova);
+            t.ObradiGađanje(RezultatGađanja.Potopljen);
+            Assert.AreEqual(TaktikaGađanja.Nasumično, t.TaktikaGađanja);
+        }
+
+        [TestMethod]
+        public void Topništvo_NakonPrvogPogotkaTaktikaPrelaziUKružnu()
+        {
+            int redaka = 5;
+            int stupaca = 5;
+            int[] duljineBrodova = new int[] { 3, 2, 1 };
+            Topništvo t = new Topništvo(redaka, stupaca, duljineBrodova);
+            t.ObradiGađanje(RezultatGađanja.Pogodak);
+            Assert.AreEqual(TaktikaGađanja.Kružno, t.TaktikaGađanja);
+        }
+
+        [TestMethod]
+        public void Topništvo_KružnoGađanjeNakonPromašajaOstajeKružno()
+        {
+            int redaka = 5;
+            int stupaca = 5;
+            int[] duljineBrodova = new int[] { 3, 2, 1 };
+            Topništvo t = new Topništvo(redaka, stupaca, duljineBrodova);
+            t.ObradiGađanje(RezultatGađanja.Pogodak);
+            t.ObradiGađanje(RezultatGađanja.Promašaj);
+            Assert.AreEqual(TaktikaGađanja.Kružno, t.TaktikaGađanja);
+        }
+
+        [TestMethod]
+        public void Topništvo_KružnoGađanjeNakonPogotkaPrelaziULinijsko()
+        {
+            int redaka = 5;
+            int stupaca = 5;
+            int[] duljineBrodova = new int[] { 3, 2, 1 };
+            Topništvo t = new Topništvo(redaka, stupaca, duljineBrodova);
+            t.ObradiGađanje(RezultatGađanja.Pogodak);
+            t.ObradiGađanje(RezultatGađanja.Pogodak);
+            Assert.AreEqual(TaktikaGađanja.Linijsko, t.TaktikaGađanja);
+        }
+
+        [TestMethod]
+        public void Topništvo_KružnoGađanjeNakonPotonućaPrelaziUNasumično()
+        {
+            int redaka = 5;
+            int stupaca = 5;
+            int[] duljineBrodova = new int[] { 3, 2, 1 };
+            Topništvo t = new Topništvo(redaka, stupaca, duljineBrodova);
+            t.ObradiGađanje(RezultatGađanja.Pogodak);
+            t.ObradiGađanje(RezultatGađanja.Potopljen);
+            Assert.AreEqual(TaktikaGađanja.Nasumično, t.TaktikaGađanja);
+        }
+
+        [TestMethod]
+        public void Topništvo_LinijskoGađanjeNakonPromašajaOstajeLinijsko()
+        {
+            int redaka = 5;
+            int stupaca = 5;
+            int[] duljineBrodova = new int[] { 3, 2, 1 };
+            Topništvo t = new Topništvo(redaka, stupaca, duljineBrodova);
+            t.ObradiGađanje(RezultatGađanja.Pogodak);
+            t.ObradiGađanje(RezultatGađanja.Pogodak);
+            t.ObradiGađanje(RezultatGađanja.Promašaj);
+            Assert.AreEqual(TaktikaGađanja.Linijsko, t.TaktikaGađanja);
+        }
+
+        [TestMethod]
+        public void Topništvo_LinijskoGađanjeNakonPotapanjaPostajeNasumično()
+        {
+            int redaka = 5;
+            int stupaca = 5;
+            int[] duljineBrodova = new int[] { 3, 2, 1 };
+            Topništvo t = new Topništvo(redaka, stupaca, duljineBrodova);
+            t.ObradiGađanje(RezultatGađanja.Pogodak);
+            t.ObradiGađanje(RezultatGađanja.Pogodak);
+            t.ObradiGađanje(RezultatGađanja.Pogodak);
+            t.ObradiGađanje(RezultatGađanja.Potopljen);
+            Assert.AreEqual(TaktikaGađanja.Nasumično, t.TaktikaGađanja);
+        }
+    }
+}
