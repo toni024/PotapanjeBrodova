dbc9971772f505775db1a14909a8bfe3a212c549
Julijan Šribar <julijan.sribar@gmail.com>
2017-05-16 09:32:22 +0200

    Početna implementacija SlučajnogPucača.

--- PotapanjeBrodova/PotapanjeBrodova/SlučajniPucač.cs
+++ PotapanjeBrodova/PotapanjeBrodova/SlučajniPucač.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -15,24 +16,46 @@ namespace PotapanjeBrodova
 
         public Polje Gađaj()
         {
-            throw new NotImplementedException();
+            var kandidati = DajKandidate();
+            Debug.Assert(kandidati.Count > 0);
+            gađanoPolje = kandidati[izbornik.Next(kandidati.Count)];
+            return gađanoPolje;
         }
 
         public void ObradiGađanje(RezultatGađanja rezultat)
         {
-            throw new NotImplementedException();
+            switch (rezultat)
+            {
+                case RezultatGađanja.Promašaj:
+                    return;
+                case RezultatGađanja.Pogodak:
+                    mreža.UkloniPolje(gađanoPolje);
+                    return;
+                case RezultatGađanja.Potopljen:
+                    Debug.Assert(false, "Za sada nisu predviđeni brodovi duljine 1 pa SlučajniPucač ne bi smio potopiti brod.");
+                    break;
+                default:
+                    Debug.Assert(false, $"Neočekivana vrijednost: {rezultat}.");
+                    break;
+            }
         }
 
         public IEnumerable<Polje> PogođenaPolja
         {
             get
             {
-                throw new NotImplementedException();
+                return new Polje[] { gađanoPolje };
             }
+        }
+
+        private List<Polje> DajKandidate()
+        {
+            return mreža.DajNizoveSlobodnihPolja(duljinaBroda).SelectMany(niz => niz).ToList();
         }
 
         private Mreža mreža;
         private int duljinaBroda;
-
+        private Polje gađanoPolje;
+        private Random izbornik = new Random();
     }
 }
